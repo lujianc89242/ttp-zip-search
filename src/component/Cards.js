59147416963f223e8909c91af8cbd76fbf6ca1fe
Roger Lu <rogerfrontend@gmail.com>
2019-01-09 10:43:38 -0500

    stating component

---
+++ src/component/Cards.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './Cards.css';
+
+class Cards extends Component{
+  constructor(props){
+    this.state = {
+      data:[
+        {
+          State: "State 1",
+          Location: "Location 1",
+          Population: "Population 1",
+          Totalwages: "Total wages 1",
+        },
+        {
+          State: "State 2",
+          Location: "Location 2",
+          Population: "Population 2",
+          Totalwages: "Total wages 2",
+        },
+        {
+          State: "State 3",
+          Location: "Location 3",
+          Population: "Population 3",
+          Totalwages: "Total wages 3",
+        }
+      ]
+    }
+  }
+  render(){
+    return();
+  }
+}
