fcb23428fe564d3570f654c13aff16909c08a8e4
Roger Lu <rogerfrontend@gmail.com>
2019-01-09 12:27:01 -0500

    add search bar

--- src/App.js
+++ src/App.js
@@ -4,16 +4,50 @@ import './App.css';
 import Cards from './component/Cards'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      zipCode: '',
+      edit: false
+    };
+  }
+  handleChange(e){
+    this.setState( {zipCode: e.target.value});
+
+  }
+  handleSearch(e){
+    e.preventDefault();
+    this.setState( {edit: true} );
+  }
   render() {
-    return (
-      <div className="App">
+    if(this.state.edit){
+      return (
+        <div className="App">
+          <header className="App-header">
+              <h2>Zip Code Search1</h2>
+
+              </header>
+              zipcode:
+              <input type="text" onChange= {this.handleChange.bind(this)} onSubmit={this.handleSearch.bind(this) }/>
+              <Cards />
+        </div>
+      );
+    }
+    else{
+      return(
+        <div className="App">
         <header className="App-header">
-                <h2>Zip Code Search</h2>
+                <h2>Zip Code Search2</h2>
 
             </header>
-            <Cards />
-      </div>
-    );
+            zipcode:
+            <input type="text" onSubmit={this.handleSearch.bind(this)} />
+            <button onClick={this.handleSearch.bind(this)}>Search</button>
+      </div>);
+
+
+    }
+
   }
 }
 
